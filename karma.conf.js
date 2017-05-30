864ba2dc3f4997ca444def7356a64ca71cf76dd0
Svitlana <sshabaranska@n-ix.com>
2017-05-30 20:33:21 +0300

    Refactoring

--- karma.conf.js
+++ karma.conf.js
@@ -7,11 +7,8 @@ module.exports = function (config) {
         // base path that will be used to resolve all patterns (eg. files, exclude)
         basePath: '',
 
-
         // frameworks to use
-        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
         frameworks: ['jasmine'],
-
 
         // list of files / patterns to load in the browser
         files: [
@@ -24,9 +21,6 @@ module.exports = function (config) {
             'dev/vendor/angular-mocks.js',
             'dev/app/app.module.js',
             'dev/app/components/home/home.module.js',
-            'dev/app/components/home/home.config.js',
-            'dev/app/components/home/home.controller.js',
-            'dev/app/components/home/home.service.js',
             'dev/app/shared/navigation-bar/navigation-bar.module.js',
             'dev/app/shared/trust-url/trust-url.module.js',
             'dev/app/**/*.js',
@@ -40,17 +34,13 @@ module.exports = function (config) {
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
-        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
         reporters: ['progress'],
-
 
         // web server port
         port: 9876,
 
-
         // enable / disable colors in the output (reporters and logs)
         colors: true,
-
 
         // level of logging
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
@@ -60,11 +50,8 @@ module.exports = function (config) {
         // enable / disable watching file and executing tests whenever any file changes
         autoWatch: true,
 
-
         // start these browsers
-        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         browsers: ['Chrome'],
-
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ require('./server.routes.js')(app);
 // application -------------------------------------------------------------
 app.get('*', function (req, res) {
     res.sendFile(__dirname + '../dev/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-    });
+});
 
 
 // listen (start app with node server.js) ======================================
